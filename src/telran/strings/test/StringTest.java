61e6d806e35a4354c841041bb068926b0cd25b09
Barso2 <reznitskyalex@gmail.com>
2023-05-24 00:38:23 +0300

    HW-1 solution

--- src/telran/strings/test/StringTest.java
+++ src/telran/strings/test/StringTest.java
@@ -25,9 +25,44 @@ class StringTest {
 		assertFalse(str.contains("lelo"));
 
 	}
+	//HW_1
 	@Test
 	void compareToTest() {
 		//TODOooo
 		String str = "Hello";
+		assertEquals(-32,str.compareTo("hello"));
+		assertEquals(0,str.compareTo("Hello"));
+		assertEquals(32,str.compareTo("HellO"));
+		assertEquals(32,str.compareTo("HELLO"));
+		assertEquals(-3,str.compareTo("Helloooo"));
+		assertEquals(3,str.compareTo("He"));
+	}
+	@Test
+	void concatTest() {
+		String str = "John";
+		String str2 = " Wick";
+		assertEquals("John Wick",str.concat(str2));
+		assertEquals("John1",str.concat("1"));
+		assertEquals("John,",str.concat(","));
+	}
+	@Test
+	void endsWithTest() {
+		String str = "Hello";
+		assertTrue(str.endsWith("lo"));
+		assertFalse(str.endsWith("LO"));
+		assertFalse(str.endsWith(" "));
+	}
+	@Test
+	void lengthTest() {
+		String str = "Hello";
+		String str2 = "";
+		assertEquals(5,str.length());
+		assertEquals(0,str2.length());
+	}
+	@Test
+	void replaceTest() {
+		String str = "Hello";
+		assertEquals("Heppa",str.replace("llo", "ppa"));
+		assertEquals("Hollo",str.replace('e', 'o'));
 	}
 }
